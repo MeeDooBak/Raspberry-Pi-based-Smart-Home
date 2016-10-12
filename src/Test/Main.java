1868387dd6d6b9eec8250ac5d7318b144e336594
CPIT499 <mkb_2011@hotmail.com>
2016-10-12 16:26:35 +0300

    fourth commit - add IR Sensor With Alarm

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -1,7 +1,10 @@
 package Test;
 
 import Relay.Relay;
+import com.adventnet.snmp.snmp2.SnmpAPI;
 import com.pi4j.io.gpio.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
@@ -11,25 +14,21 @@ public class Main {
         System.out.println("<--Pi4J--> GPIO Control Example ... started.");
 
         GpioController gpio = GpioFactory.getInstance();
-        GpioPinDigitalInput pin27 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "PIN 27");// IR Receiver PIN 27
-        GpioPinDigitalOutput pin4 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_07, "PIN 4");// IR LED PIN 7
+        GpioPinDigitalInput pin17 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, "PIN 17");// IR Receiver PIN 17
         GpioPinDigitalOutput pin18 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PIN 18");// LED PIN 18
 
         command = new Relay("192.168.1.2", 161, "private");
-
         while (true) {
-            pin4.high();
-            if (pin27.isHigh()) {
-                pin18.high();
+            if (pin17.isLow()) {
+                pin18.low();
                 System.out.println("Good");
-                //command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "0");
+                command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "0");
 
             } else {
-                //command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "1");
-                pin18.low();
+                command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "1");
+                pin18.high();
                 System.out.println("Bad");
             }
-            pin4.low();
         }
     }
 }
