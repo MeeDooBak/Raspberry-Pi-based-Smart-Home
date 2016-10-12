33cf75f53fdd4969791765fd9444f98f8728fd9c
CPIT499 <mkb_2011@hotmail.com>
2016-10-12 22:25:43 +0300

    fourth commit - add IR Sensor With Alarm

--- src/Test/Main.java
+++ src/Test/Main.java
@@ -3,12 +3,13 @@ package Test;
 import Relay.Relay;
 import com.adventnet.snmp.snmp2.SnmpAPI;
 import com.pi4j.io.gpio.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Main {
 
+    public static int num = 0;
+    public static boolean change;
     public static Relay command;
+    public static GpioPinDigitalOutput pin18;
 
     public static void main(String[] args) {
         System.out.println("<--Pi4J--> GPIO Control Example ... started.");
@@ -17,18 +18,35 @@ public class Main {
         GpioPinDigitalInput pin17 = gpio.provisionDigitalInputPin(RaspiPin.GPIO_00, "PIN 17");// IR Receiver PIN 17
         GpioPinDigitalOutput pin18 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "PIN 18");// LED PIN 18
 
-        command = new Relay("192.168.1.2", 161, "private");
+        command = new Relay("192.168.1.15", 161, "private");
         while (true) {
             if (pin17.isLow()) {
-                pin18.low();
-                System.out.println("Good");
-                command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "0");
-
+                if (num == 1) {
+                    System.out.println("Good");
+                    pin18.low();
+                    num = 0;
+                    change = true;
+                    new Thread(Change).start();
+                }
             } else {
-                command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "1");
-                pin18.high();
-                System.out.println("Bad");
+                if (num == 0) {
+                    System.out.println("Bad");
+                    pin18.high();
+                    num = 1;
+                    change = false;
+                    new Thread(Change).start();
+                }
             }
         }
     }
+    private static final Runnable Change = new Runnable() {
+        @Override
+        public void run() {
+            if (change) {
+                command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "0");
+            } else {
+                command.SNMP_SET(".1.3.6.1.4.1.19865.1.2.1." + 1 + ".0", SnmpAPI.INTEGER, "1");
+            }
+        }
+    };
 }
